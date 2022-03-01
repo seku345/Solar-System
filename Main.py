09a4a6ad3b34244c77f4ffcb06a20ba5d8185801
seku345 <seku345@yandex.ru>
2022-03-01 20:40:55 +0300

    main

--- Main.py
+++ Main.py
@@ -3,4 +3,42 @@ from pygame import *
 from math import *
 from win32api import GetSystemMetrics
 
-# Настройка экрана
+# Класс планет
+class Planet(sprite.Sprite):
+
+    ...
+
+# Главная функция
+def main():
+    
+    # Настройка экрана
+    H = GetSystemMetrics(0)
+    V = GetSystemMetrics(1)
+
+    window = display.set_mode((0, 0), FULLSCREEN)
+    display.set_caption('Planet Simulation')
+    background = transform.scale(image.load('Sprites/Background.jpg'), (H, V))
+
+    clock = time.Clock()
+
+    # Основной цикл
+    run = True
+    while run:
+
+        # Кадры в секунду
+        clock.tick(60)
+        display.update()
+
+        # Фон
+        window.blit(background, (0, 0))
+
+        # Обработка событий
+        for e in event.get():
+
+            # Закрытие окна
+            if e.type == KEYDOWN and e.key == K_ESCAPE:
+                run = False
+
+# Запуск
+if __name__ == '__main__':
+    main()
