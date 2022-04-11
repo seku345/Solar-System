55e07b499b1ba2f8384053d6d14a7fa02fd02601
seku345 <seku345@yandex.ru>
2022-04-11 16:04:14 +0300

    main

--- Main.py
+++ Main.py
@@ -13,10 +13,10 @@ class Planet(sprite.Sprite):
     TIMESTEP = 3600 * 24 # Шаг времени в: 1 секунда равна 1 суткам
 
     # Инициализация
-    def __init__(self, planet_image, x, y, radius, mass, sun):
+    def __init__(self, x, y, radius, planet_image, mass, sun= False):
         super().__init__()
 
-        self.image = transform_scale(image.load(planet_image), (self.radius, self.radius))
+        self.image = transform.scale(image.load(planet_image), (self.radius, self.radius))
         self.x = x
         self.y = y
         self.radius = radius
@@ -52,8 +52,35 @@ def main():
     clock = time.Clock()
 
     # Создание планет
-    sun = Planet(0, 0, 30, ...)
+    sun = Planet(0, 0, 30, 'Sprites/Sun.png', 1.98892 * 10**30, True)
 
+    mercury = Planet(0.387 * Planet.AU, 0, 8, 'Sprites/Mercury.png', 3.30 * 10**23)
+    mercury.y_vel = -47.4 * 1000
+
+    venus = Planet(0.723 * Planet.AU, 0, 14, 'Sprites/Venus.png', 4.8685 * 10**24)
+    venus.y_vel = -35.02 * 1000
+
+    earth = Planet(-1 * Planet.AU, 0, 16, 'Sprites/Earth.png', 5.9742 * 10**24)
+    earth.y_vel = 29.783 * 1000
+    
+    mars = Planet(-1.524 * Planet.AU, 0, 12, 'Sprites/Mars.png', 6.39 * 10**23)
+    mars.y_vel = 24.077 * 1000
+    
+    jupiter = Planet(5.204 * Planet.AU, 0, 14, 'Sprites/Jupiter.png', 4.8685 * 10**24)
+    jupiter.y_vel = -35.02 * 1000
+
+    saturn = Planet(9.54 * Planet.AU, 0, 14, 'Sprites/Saturn.png', 4.8685 * 10**24)
+    saturn.y_vel = -35.02 * 1000
+    
+    uranus = Planet(18.4 * Planet.AU, 0, 14, 'Sprites/Uranus.png', 4.8685 * 10**24)
+    uranus.y_vel = -35.02 * 1000
+
+    neptune = Planet(30 * Planet.AU, 0, 14, 'Sprites/Neptune.png', 4.8685 * 10**24)
+    neptune .y_vel = -35.02 * 1000
+
+    pluto = Planet(39.53 * Planet.AU, 0, 14, 'Sprites/Pluto.png', 4.8685 * 10**24)
+    pluto.y_vel = -35.02 * 1000
+    
     # Основной цикл
     run = True
     while run:
